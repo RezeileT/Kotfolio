e72e684c524fd82ff1de675d6f773b3ae4a110de
Dfenrisf <fenrisf.mini@gmail.com>
2025-12-19 17:28:18 +0100

    Cambios de información personal

--- site/src/jsMain/kotlin/com/binayshaw7777/kotfolio/utils/Constants.kt
+++ site/src/jsMain/kotlin/com/binayshaw7777/kotfolio/utils/Constants.kt
@@ -1,38 +1,41 @@
 package com.binayshaw7777.kotfolio.utils
 
 object Constants {
-    const val RESUME_URL = "https://drive.google.com/file/d/1cDrT_MfkUja5_xXnK5JvD5esT7MwFara/view?usp=sharing"
-    const val LINKEDIN_URL = "https://www.linkedin.com/in/binayshaw7777/"
-    const val GITHUB_URL = "https://github.com/binayshaw7777"
+    const val RESUME_URL = "https://drive.google.com/file/d/1h60hfu_QT-otKAW-BcmmS452kgRdJ8x0/view?usp=drive_link"
+    const val LINKEDIN_URL = "www.linkedin.com/in/sebastián-barrios-mármol-221a61341"
+    const val GITHUB_URL = "https://github.com/RezeileT"
     const val TWITTER_URL = "https://twitter.com/binayplays7777"
     const val BEHANCE_URL = "https://www.behance.net/GAMIX7"
     const val MEDIUM_URL = "https://medium.com/@binayshaw7777"
-    const val MAIL_TO = "mailto:binayshaw7777@gmail.com"
+    const val MAIL_TO = "mailto:sebastianbarrios1812@gmail.com"
     const val PROJECT_READBUD_URL = "https://github.com/binayshaw7777/ReadBud"
     const val PROJECT_JUSTAP_URL = "https://github.com/binayshaw7777/JusTap"
     const val PROJECT_MEDIFY_URL = "https://github.com/binayshaw7777/Medify"
     const val PROJECT_PASSGENIE_URL = "https://github.com/binayshaw7777/PassGinie-Kobweb"
     const val PROJECT_PRESIN_URL = "https://github.com/binayshaw7777/Microsoft-Engage-2022-PresIN"
 
-    const val HELLO_IM = "Hello I'm"
-    const val BINAY_SHAW = "Binay Shaw."
-    const val AND_NICE_TO_MEET_YOU = "and Nice to meet you!"
-    const val RESUME = "Resume"
+    const val HELLO_IM = "Hola, soy"
+    const val BINAY_SHAW = "Sebastián Barrios."
+    const val AND_NICE_TO_MEET_YOU = "y es un placer conocerte."
+    const val RESUME = "Currículum"
 
-    const val ABOUT_ME_TEXT = "Binay is a versatile Software Developer proficient in both Android and iOS development. Additionally, he demonstrates a hands-on approach to crafting elegant User Interfaces, showcasing a talent for bug resolution and consistently delivering optimal results."
-    const val ABOUT_ME_TITLE = "About me."
+    const val ABOUT_ME_TEXT =
+        "Soy es un desarrollador de software versátil, competente tanto en desarrollo Android como iOS. Además, demostrando un enfoque práctico para crear interfaces de usuario elegantes, mostrando talento para la resolución de errores y ofreciendo siempre resultados óptimos."
+    const val ABOUT_ME_TITLE = "Sobre mí."
 
-    const val EXPERIENCE_TITLE = "Work Experience."
-    const val EXPERIENCE_SECTION_TEXT = "I’m a Software Developer Engineer, specializing in Android & Backend Development with "
-    const val FIVE_MONTH = "5 months"
-    const val OF_INTERNSHIP = " of Internship Experience."
+    const val EXPERIENCE_TITLE = "Experiencia laboral."
+    const val EXPERIENCE_SECTION_TEXT =
+        "Soy un desarrollador software, especializado en desarrollo Android y frontend con "
+    const val FIVE_MONTH = "5 meses"
+    const val OF_INTERNSHIP = " de experiencia como becario."
 
-    const val SKILLS_AND_TOOLS_TITLE = "Skills & Tools."
+    const val SKILLS_AND_TOOLS_TITLE = "Habilidades y herramientas."
 
-    const val PHOTOGRAPH_TITLE = "Sometimes I love to click."
+    const val PHOTOGRAPH_TITLE = "A veces me encanta hacer fotos."
 
-    const val PROJECT_TITLE = "My Side Projects."
-    const val PROJECT_SECTION_TEXT = "I really have a bad habit of creating projects over projects XD"
-    const val PROJECT_REPOSITORIES_URL = "https://github.com/binayshaw7777?tab=repositories"
-    const val MORE_ON_GITHUB = "More on GitHub."
+    const val PROJECT_TITLE = "Mis proyectos personales."
+    const val PROJECT_SECTION_TEXT =
+        "Tengo la mala costumbre de crear proyecto tras proyecto XD"
+    const val PROJECT_REPOSITORIES_URL = "https://github.com/RezeileT?tab=repositories"
+    const val MORE_ON_GITHUB = "Más en GitHub."
 }
